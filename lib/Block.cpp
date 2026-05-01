376a7d30adce1285d3e80401473bd9652933b461
Krizsan0596 <kristof.nagy.lazar@proton.me>
2026-05-01 22:22:25 +0000

    fix: correct memory comparison in Block write method and improve Inode write logic

--- lib/Block.cpp
+++ lib/Block.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <memory>
 #include <stdexcept>
+#include <cstring>
 
 Block::Block(const std::string& Data) : data{} {
     if (Data.size() > BLOCK_SIZE)
@@ -14,7 +15,7 @@ std::string Block::read() const {
 }
 
 std::shared_ptr<Block> Block::write(const char data[BLOCK_SIZE]) {
-    if (data == this->data) return shared_from_this();
+    if (std::memcmp(data, this->data, BLOCK_SIZE) == 0) return shared_from_this();
     std::shared_ptr<Block> new_block = std::make_shared<Block>(std::string(data, BLOCK_SIZE));
     return new_block;
 }
